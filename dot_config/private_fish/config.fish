fb777775a68ff100008247114197f463d3f12827
Alexander L <ropptar@ya.ru>
2025-04-19 07:45:41 +0300

    laptop

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Hydro Prompt
+	set --universal hydro_color_pwd 98971a 
+	set --universal hydro_color_git 458588 
+	set --universal hydro_color_prompt ebdbb2
+	set --universal hydro_color_error cc241d
+	set --universal hydro_color_duratuion 928374 
+
+	# Abbreviations
+	abbr -a -- wtf tldr
+	abbr -a -- chez chezmoi
 end
